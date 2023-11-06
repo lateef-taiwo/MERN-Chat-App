67c40762f525311983134d928a2255e93b550b86
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-11-06 09:25:58 +0100

    Update frontend dependencies

--- frontend/src/components/ScrollableChat.jsx
+++ frontend/src/components/ScrollableChat.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { chatState } from "../Context/ChatProvider";
 import ScrollableFeed from "react-scrollable-feed";
 import {
@@ -7,7 +10,7 @@ import {
   isSameSenderMargin,
   isSameUser,
 } from "../config/ChatLogics";
-import { Avatar, Menu, MenuButton, MenuItem, MenuList, Tooltip } from "@chakra-ui/react";
+import { Avatar, MenuButton, MenuItem, MenuList, Tooltip } from "@chakra-ui/react";
 
 const ScrollableChat = ({ messages }) => {
   const { user } = chatState();
@@ -16,8 +19,8 @@ const ScrollableChat = ({ messages }) => {
     <ScrollableFeed>
       {messages &&
         messages.map((m, i) => (
-          <div key={m._id}>
-            <div style={{ display: "flex" }}>
+          <div key={m._id} className="overflow-visible relative">
+            <div style={{ display: "flex" }} className="relative">
               {(isSameSender(messages, m, i, user._id) ||
                 isLastMessage(messages, i, user._id)) && (
                 <Tooltip
@@ -49,8 +52,96 @@ const ScrollableChat = ({ messages }) => {
                 className="relative"
               >
                 <p>{m.content}</p>
-                <div className="top-0 right-0 absolute">
-                  <Menu>
+                <div className=" top-0 right-0 absolute">
+                  <Menu as="div" className="relative inline-block text-left">
+                    <div>
+                      <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                        Options
+                        <ChevronDownIcon
+                          className="-mr-1 h-5 w-5 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </Menu.Button>
+                    </div>
+
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95"
+                    >
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <div className="py-1">
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 text-gray-900"
+                                    : "text-gray-700",
+                                  "block px-4 py-2 text-sm"
+                                )}
+                              >
+                                Account settings
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 text-gray-900"
+                                    : "text-gray-700",
+                                  "block px-4 py-2 text-sm"
+                                )}
+                              >
+                                Support
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <Menu.Item>
+                            {({ active }) => (
+                              <a
+                                href="#"
+                                className={classNames(
+                                  active
+                                    ? "bg-gray-100 text-gray-900"
+                                    : "text-gray-700",
+                                  "block px-4 py-2 text-sm"
+                                )}
+                              >
+                                License
+                              </a>
+                            )}
+                          </Menu.Item>
+                          <form method="POST" action="#">
+                            <Menu.Item>
+                              {({ active }) => (
+                                <button
+                                  type="submit"
+                                  className={classNames(
+                                    active
+                                      ? "bg-gray-100 text-gray-900"
+                                      : "text-gray-700",
+                                    "block w-full px-4 py-2 text-left text-sm"
+                                  )}
+                                >
+                                  Sign out
+                                </button>
+                              )}
+                            </Menu.Item>
+                          </form>
+                        </div>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                  {/* <Menu className="" style={{ position: "relative" }}>
                     <MenuButton>
                       <span
                         style={{
@@ -63,10 +154,10 @@ const ScrollableChat = ({ messages }) => {
                         expand_more
                       </span>
                     </MenuButton>
-                    <MenuList>
+                    <MenuList className="absolute right-[-24px]">
                       <MenuItem>Edit</MenuItem>
                     </MenuList>
-                  </Menu>
+                  </Menu> */}
                 </div>
                 <div className="flex mt-[-10px] justify-between">
                   <div className="ml-auto">
